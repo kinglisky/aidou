1ff2461823ebe17a5cf98d8487196f0f76defb4a
芊野 <free.nan.sky@gmail.com>
2017-11-14 23:15:48 +0800

    Test

--- src/background/config.ts
+++ src/background/config.ts
@@ -1,10 +1,14 @@
-export const API = 'https://pic.sogou.com/pics/json.jsp'
-export const DEFAULT_PARAMS = {
+// 表情包的搜索接口
+const API = 'https://pic.sogou.com/pics/json.jsp'
+
+// 默认参数
+const DEFAULT_PARAMS = {
   query: '',
   st: 5,
   start: 0,
   xml_len: 100
 }
+
 export default {
   API, DEFAULT_PARAMS
 }
---
+++ src/background/util.ts
@@ -0,0 +1,10 @@
+interface Params {
+  query: string,
+  st: number,
+  start: number,
+  xml_len: number
+}
+
+export function serialize(params: Params): string {
+  return Object.keys(params).map(key => `${key}=${params[key]}`).join('&')
+}
